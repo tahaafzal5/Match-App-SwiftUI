4a7b3784cc81714201f7f970bc9899018eb05cdf
Taha Afzal <tahaafzal5@hotmail.com>
2020-08-09 15:47:10 -0500

    Lecture 1

--- Match App SwiftUI/ContentView.swift
+++ Match App SwiftUI/ContentView.swift
@@ -9,13 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    var body: some View { 
+        
+        HStack {
+            
+            ForEach(0 ..< 3) { item in
+                CardView(isFaceUp: true)
+            }
+            
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    
+    var body: some View {
+        
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3)
+                    
+                Text("👻")
+            }
+            else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
+        
     }
 }
